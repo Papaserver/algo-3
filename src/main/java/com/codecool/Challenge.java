1194329fb1ef7ee78038d68f798f7b055c29839f
Sipkó Miklós <miklossipko@gmail.com>
2021-04-07 11:10:44 +0200

    All tests passed!

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -3,6 +3,11 @@ package com.codecool;
 public class Challenge {
 
     public static int minPositive(int a, int b) {
-
+        if (a <= 0 && b <= 0) {
+            return -1;
+        } else if (a <= 0 || b <= 0) {
+            return a <= 0 ? b : a;
+        }
+        return a > b ? b : a;
     }
 }
